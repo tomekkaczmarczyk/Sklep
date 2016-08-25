40aa6555a438fe25985d0967b9f6e877366689cc
mkuncicka <mkuncicka@gmail.com>
2016-08-25 09:22:50 +0200

    Css added, redirections on main site changed

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <script src="js/jquery-2.2.4.min.js"></script>
 <!--        <script src="js/app.js"></script>-->
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="web/css/style.css">
     </head>
     <body>
     <?php
@@ -14,25 +15,64 @@
     require_once "src/dbConnection.php";
     $conn = connectToDataBase();
     session_start();
+
+    $loggedUser = false;
+    if (isset($_SESSION['user_id'])) {
+        $id = $_SESSION['user_id'];
+        $loggedUser = User::getUser($conn, $id);
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['mail']) and isset($_POST['password'])) {
+            $email = $_POST['mail'];
+            $password = $_POST['password'];
+            $loggedUser = User::logIn($conn, $email, $password);
+            if ($loggedUser) {
+                $_SESSION['user_id'] = $loggedUser->getId();
+            } else {
+                echo "Błędne dane logowania.<br>";
+            }
+        }
+    }
+
     ?>
-    <div>
+    <div class="logdata">
+        <?php
+        if ($loggedUser) {
+            echo "Zalogowano jako: " . $loggedUser->getMail();
+        }
+        ?>
         <form method="post">
             <button type="submit" name="logout">Wyloguj</button>
         </form>
     </div>
-    <div>
-        <a href="index.php">Strona główna</a>
+    <div class="menu">
+        <ul>
+            <li><a href="index.php">Strona główna</a></li>
+            <?php
+        if($loggedUser) {
+            if ($loggedUser->getIsAdmin()==1) {
+                redirect('index.php?action=sites/admin_panel');
+            } else {
+                echo "<li><a href='index.php?action=sites/order_site'>Twoje zamówienia</a></li>";
+                echo "<li><a href='index.php?action=sites/basket'>Twój koszyk</a></li>";
+                echo "<li><a href='index.php?action=sites/user_info'>Informacje o Tobie</a></li>";
+            }
+        }
+        ?>
+        </ul>
     </div>
-    <table>
+    <div class="categories">
         <p>Kategorie:</p>
+        <ul>
         <?php
         $categories = Item::getAllCategories($conn);
         foreach ($categories as $cat) {
             $url = "index.php?action=sites/items_site&category=" . $cat;
-            echo "<tr><td><a href='" . $url . "'>" . $cat . "</a></td></tr>";
+            echo "<li><a href='" . $url . "'>" . $cat . "</a></li>";
         }
         ?>
-    </table>
+        </ul>
+    </div>
+    <div class="container">
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST' and isset($_POST['logout'])) {
         unset($_SESSION['user_id']);
@@ -47,12 +87,10 @@
     } else {
         require 'sites/login.php';
     }
-?>
+    //var_dump($_POST);
+    var_dump($_GET);
+    //var_dump($_SESSION);
+    ?>
+    </div>
     </body>
 </html>
-
-<?php
-//var_dump($_POST);
-var_dump($_GET);
-//var_dump($_SESSION);
-?>
--- sites/login.php
+++ sites/login.php
@@ -23,34 +23,3 @@
     </table>
     <p><a href="index.php?action=sites/register">Nie masz konta? Zarejestruj się!</a></p>
 </form>
-
-<?php
-$loggedUser = false;
-if (isset($_SESSION['user_id'])) {
-    $id = $_SESSION['user_id'];
-    $loggedUser = User::getUser($conn, $id);
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['mail']) and isset($_POST['password'])) {
-        $email = $_POST['mail'];
-        $password = $_POST['password'];
-        $loggedUser = User::logIn($conn, $email, $password);
-        if ($loggedUser) {
-            $_SESSION['user_id'] = $loggedUser->getId();
-        } else {
-            echo "Błędne dane logowania.<br>";
-        }
-    }
-}
-
-if($loggedUser) {
-    if ($loggedUser->getIsAdmin()==1) {
-        redirect('index.php?action=sites/admin_panel');
-    } else {
-        echo "Zalogowano jako: " . $loggedUser->getMail();
-        echo "<p><a href='index.php?action=sites/order_site'>Twoje zamówienia</a></p>";
-        echo "<p><a href='index.php?action=sites/basket'>Twój koszyk</a></p>";
-        echo "<p><a href='index.php?action=sites/user_info'>Informacje o Tobie</a></p>";
-
-    }
-}
-?>
